18c41bfec34b419dcbb50e2141c57db7c3290dad
mcpars <157234335+mcpars@users.noreply.github.com>
2026-03-19 20:56:23 -0400

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -32,8 +32,8 @@ contract Destination is AccessControl {
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		address underlying = wrapped_tokens[_wrapped_token];
 		//require(underlying != address(0), "Wrapped token not registered");
-		BridgeToken(_wrapped_token).burn(msg.sender, _amount);
-		emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+		//BridgeToken(_wrapped_token).burn(msg.sender, _amount);
+		//emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {

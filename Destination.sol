1df9d413453c2758fc5f691ac29f387685cad7de
mcpars <157234335+mcpars@users.noreply.github.com>
2026-03-20 15:00:20 -0400

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -25,15 +25,17 @@ contract Destination is AccessControl {
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		address wrapped = wrapped_tokens[_underlying_token];
 		require(wrapped != address(0), "no token");
-		BridgeToken(wrapped).mint(_recipient, _amount);
+		
 		emit Wrap(_underlying_token, wrapped, _recipient, _amount);
+		BridgeToken(wrapped).mint(_recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		address underlying = underlying_tokens[_wrapped_token];
 		require(underlying != address(0), "wrapped token error");
-		BridgeToken(_wrapped_token).burn(_amount);
+		
 		emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+		BridgeToken(_wrapped_token).burn(_amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {

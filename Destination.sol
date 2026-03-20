91740f55ffd19c51d089faa679a7bd64efb41eab
mcpars <157234335+mcpars@users.noreply.github.com>
2026-03-20 13:54:08 -0400

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -30,7 +30,7 @@ contract Destination is AccessControl {
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		address underlying = wrapped_tokens[_wrapped_token];
+		//address underlying = wrapped_tokens[_wrapped_token];
 		//require(underlying != address(0), "Wrapped token not registered");
 		//BBridgeToken(_wrapped_token).burn(msg.sender, _amount);
 		//emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
